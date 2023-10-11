3188efa66bf7c6e823f0dcca3a42264280f68fad
LEE_CO_DE <147568981+LEECODE247@users.noreply.github.com>
2023-10-11 17:18:58 +0900

    Update test1.java
    git fixed

--- githubTest/src/githubTest/test1.java
+++ githubTest/src/githubTest/test1.java
@@ -3,6 +3,8 @@ package githubTest;
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println("hello, java");
+		System.out.println("hello, java_copy");//git에서 고친내용
+		
 	}
 
 }
